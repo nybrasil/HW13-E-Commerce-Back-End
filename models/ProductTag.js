19d09f4f3e173ce2c45f3fcd3dc87bf58f8e2ddf
Tania Bezerra <nybrasil@gmail.com>
2024-04-25 15:43:11 -0400

    Completed ProductTag.js

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,26 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
@@ -18,3 +38,5 @@ ProductTag.init(
 );
 
 module.exports = ProductTag;
+
+
